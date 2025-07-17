b67043a2c6794934b9b5e569d6cb9e95134a9360
biancapcr <biancacristina.pocora@gmail.com>
2025-07-17 22:56:10 +0200

    [add] first array

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // 1. Array iniziale dei membri del team
+  const teamMembers = [
+    { name: "Marco Bianchi",  role: "Designer",             email: "marcobianchi@team.com", img: "assets/img/male1.png" },
+    { name: "Laura Rossi",    role: "Front-end Developer", email: "laurarossi@team.com",    img: "assets/img/female1.png" },
+    { name: "Giorgio Verdi",  role: "Back-end Developer",  email: "giorgioverdi@team.com",  img: "assets/img/male2.png" },
+    { name: "Marta Ipsum",    role: "SEO Specialist",      email: "martarossi@team.com",    img: "assets/img/female2.png" },
+    { name: "Roberto Lorem",  role: "SEO Specialist",      email: "robertolorem@team.com",  img: "assets/img/male3.png" },
+    { name: "Daniela Amet",   role: "Analyst",             email: "danielaamet@team.com",   img: "assets/img/female2.png" }
+  ];
+}
+);
